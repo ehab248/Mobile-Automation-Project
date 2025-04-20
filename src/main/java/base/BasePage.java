16b943ef8595620529d9bcccc7949beb0fbbf4db
NadaKazamel <nada.mk.174@gmail.com>
2025-04-20 04:33:29 +0200

    BasePage.java file modified

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,10 +1,6 @@
 package base;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.support.PageFactory;
-
 public class BasePage {
     protected AndroidDriver driver;
 
