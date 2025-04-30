ee3d3b701d5f1406f5e3916bac98b3aa09c3cd4a
marwanmohamed <marawanxman@gmail.com>
2025-04-30 13:30:40 +0300

    marwan

---
+++ src/main/java/pages/JsonData.java
@@ -0,0 +1,30 @@
+package pages;
+
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
+public class JsonData {
+    private static final String JSON_FILE_PATH = "src/test/resources/";
+    String jsonReader;
+    String jsonFileName;
+
+    public JsonData(String jsonFileName) {
+        this.jsonFileName = jsonFileName;
+        try {
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(JSON_FILE_PATH + jsonFileName + ".json"));
+            jsonReader = data.toJSONString();
+        } catch (Exception e) {}
+    }
+
+    // login-credentials.username
+    public String getJsonData(String jsonPath) {
+        String testData = "";
+
+            testData = JsonPath.read(jsonReader, jsonPath);
+
+        return testData;
+    }
+}
